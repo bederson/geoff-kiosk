9cf4f4f0f985cae7dd7ecea6ed6274108311e2f8
Ben Bederson <bederson@gmail.com>
2014-06-15 17:07:53 -0400

    Initial support for multiple machines

--- kiosk-server/main.py
+++ kiosk-server/main.py
@@ -20,39 +20,47 @@ import webapp2
 import urllib2
 import logging
 import json
+import socket
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
+
+class SocketHandler(webapp2.RequestHandler):
+    def get(self):
+        port = self.request.get("port")
+        if port:
+            socket_url = getSocket(port, "json")
+            self.response.write(socket_url)
     
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         side1_port = "9223"
-        data = self.getSocket(side1_port, "json")
-        dataj = json.loads(data)
-        socketUrl = ""
-        tab = dataj[0]
-        if 'id' in tab:
-            logging.debug("Success: tab = " + str(tab))
-            socketUrl = "ws://localhost:" + side1_port + "/devtools/page/" + tab['id']
-        else:
-            logging.debug("Error: tab = " + str(tab))
+        socketUrl = getSocket(side1_port, "json")
         template_values = {'side1_url': socketUrl}
         url = self.request.url
         file = url[url.index("/", 7):]
         template = JINJA_ENVIRONMENT.get_template(file)
         self.response.write(template.render(template_values))
 
-    def getSocket(self, port, title):
-        url = "http://localhost:" + port + "/" + title
-        try:
-            result = urllib2.urlopen(url)
-            data = result.read()
-            return data
-        except urllib2.URLError, e:
-            logging.debug("URL error: " + e)
+def getSocket(port, title):
+    url = "http://localhost:" + port + "/" + title
+    try:
+        result = urllib2.urlopen(url)
+        data = result.read()
+        dataj = json.loads(data)
+        socketUrl = ""
+        tab = dataj[0]
+        if 'id' in tab:
+            socketUrl = "ws://localhost:" + port + "/devtools/page/" + tab['id']
+        else:
+            logging.debug("Error: tab = " + str(tab))
+        return socketUrl
+    except urllib2.URLError, e:
+        logging.debug("URL error: " + e)
         
 app = webapp2.WSGIApplication([
+    ('/get_socket', SocketHandler),
     ('/.*', MainHandler)
 ], debug=True)
