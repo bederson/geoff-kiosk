d736a6b8f6263dddfd65bc843ec3d41a77a729d9
Ben Bederson <bederson@gmail.com>
2014-08-06 21:36:25 -0400

    server example

--- kiosk-server/js/kiosk.js
+++ kiosk-server/js/kiosk.js
@@ -1,27 +1,11 @@
-var side1_socket = "";
-
-function init_kiosk(side1_ip) {
-	side1_url = "http://" + main_ip + ":" + main_port + "/get_socket?ip=" + side1_ip + ":" + side1_port + "&port=9222";
-	$.get(side1_url, function(data) {
-		console.log(data);
-		side1_socket_url = data.replace("localhost:9222", side1_ip + ":" + side1_chrome_port);
-		console.log(side1_socket_url);
-		var side1_socket = new WebSocket(side1_socket_url);
+function navigate(remote_url, local_url) {
+	full_url = "http://" + side_ip + ":" + side_port + "/navigate?port=" + side_chrome_port;
+	full_url += "&url=" + remote_url;
+	$.get(full_url, function(data) {
+	})
+	.always(function() {
+		if (typeof local_url !== "undefined" && local_url !== null) {
+			window.location.href=local_url;
+		}
 	});
 }
-
-function navigate(url) {
-	if (side1_socket != "") {
-		msg = {'id':1001, 'method':'Page.navigate', 'params':{'url':url}};
-		side1_socket.send(JSON.stringify(msg));
-	}
-}
-
-$(document).ready(function() {
-	init_kiosk(side1_ip)
-	if (side1_socket == "") {
-		$("#info").html("<h1>Unable to connect to side pages</h1>");
-	} else {
-		$("#info").html("Successfully connected to side pages");
-	}
-});
