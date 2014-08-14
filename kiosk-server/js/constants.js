b28329738b212f04f8b16806aa11d14c02e711ef
Ben Bederson <bederson@gmail.com>
2014-08-13 20:34:48 -0400

    Updated "server" to direct to multiple sides

--- kiosk-server/js/constants.js
+++ kiosk-server/js/constants.js
@@ -1,3 +1,5 @@
-var side_ip = "192.168.1.20";
+var side_ip1 = "192.168.1.20";
+var side_ip2 = "192.168.1.21";
+var side_ip3 = "192.168.1.22";
 var side_port = "8080";
 var side_chrome_port = "9222";
--- kiosk-server/js/kiosk.js
+++ kiosk-server/js/kiosk.js
@@ -1,4 +1,13 @@
-function navigate(remote_url, local_url) {
+function navigate(side_num, remote_url, local_url) {
+	if (side_num == 1) {
+		side_ip = side_ip1;
+	} else if (side_num == 2) {
+		side_ip = side_ip2;
+	} else if (side_num == 3) {
+		side_ip = side_ip3;
+	} else {
+		alert("Can't navigate to side_ip #" + side_num);
+	}
 	full_url = "http://" + side_ip + ":" + side_port + "/navigate?port=" + side_chrome_port;
 	full_url += "&url=" + remote_url;
 	$.get(full_url, function(data) {
